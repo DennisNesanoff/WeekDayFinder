b3b9f495bf7bb82576977dc3ed577135fa58bd16
Dennis <nndenninn@gmail.com>
2020-04-19 20:32:05 +0300

    Initial Commit

---
+++ WeekDayFinder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeekDayFinder
+//
+//  Created by Dennis Nesanoff on 19.04.2020.
+//  Copyright © 2020 Dennis Nesanoff. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
